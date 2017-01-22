50343ad1780516f48fa869d6788916039ff9cb99
biancapatrizavalmeo <biancapatriza@yahoo.com>
2017-01-22 19:29:36 +0800

    changed font style
    changed the font for the header to bungee shade.
    changed the font of the p tag to quicksand.
    i put the text to the center.

--- welcome.php
+++ welcome.php
@@ -11,32 +11,46 @@
 		header("location: register.php");
 	}
 ?>
+<style type="text/css">
+	#welcome-header{
+		font-family: 'Bungee Shade', cursive ;
+	}
+	p{
+		font-family: 'Quicksand', sans-serif;
+		font-size: 24px;
+	}
+</style>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Student Register</title>
 		<link href="css/bootstrap.min.css" rel="stylesheet"/>
+		<link href="https://fonts.googleapis.com/css?family=Bree+Serif|Bungee+Shade|Indie+Flower|Lobster|Pacifico|Quicksand" rel="stylesheet">
 	</head>
 	<body>
 		
 		<div class="container">
 			<nav class="navbar navbar-light">
-				<h3 class="text-center">
+				<h1 class="text-center" id="welcome-header">
 					<?php
 						echo "Welcome, ".$firstName."!";
 					?>
 				</h3>
 			</nav>
-			<div class="col-lg-offset-4 col-lg-8">
-				<?php
-					echo "<br/><br/> Welcome, " . $firstName . " " . 
-					$lastName . "! <br/>" . 
-					"Your Student Id number is " . $studentNo . 
-					".<br/> Your email address is " . $email .
-					". <br/> Your Password is " . $password .
-					". <br/> Your birthday is " . $birthdate .
-					". <br/><br/> The time is " . date('c') . ".";
-				?>
+			<div>
+				<div class="well">
+					<p class="text-center">
+					<?php
+						echo "Welcome, <strong>" . $firstName . " " . 
+						$lastName . "</strong>! <br/>" . 
+						"Your Student Id number is<strong> " . $studentNo . 
+						"</strong>.<br/> Your email address is <strong>" . $email .
+						"</strong>. <br/> Your Password is <strong>" . $password .
+						"</strong>. <br/> Your birthday is <strong>" . $birthdate .
+						"</strong>. <br/><br/> The time is <strong>" . date('c') . "</strong>.";
+					?>
+					</p>
+				</div>
 			</div>
 		</div>
 	</body>
